b26cfeb223efd2f9ca8d0d60a36e2cc015886054
love0107 <lovlesh0107@gmail.com>
2023-09-24 23:13:31 +0530

    added useState

--- react-tailwind-css-starter-pack-main/src/App.js
+++ react-tailwind-css-starter-pack-main/src/App.js
@@ -1,12 +1,27 @@
 import "./App.css";
+import {useState} from "react"
 
 function App() {
+  const [count, setCount] = useState(0);
+  function decreaseHandler(){
+     setCount(count-1);
+  }
+  function increaseHandler(){
+    setCount(count+1);
+  }
+  function resetHandler(){
+    setCount(0);
+  }
   return (
-    <main class="flex justify-center gap-4 flex-col min-h-screen">
-      <h1 class="text-3xl text-center font-bold underline">React & Tailwind CSS Starter Pack</h1>
-      <p class="text-center text-xl">This is a starter pack for React & Tailwind CSS projects.</p>
-      <img src="https://bit.ly/3wsmzTy" alt="meme" class="mx-auto" />
-    </main>
+   <div className="w-[100vw] h-[100vh] flex flex-col justify-certer items-center bg-[#344151] gap-10">
+    <div className="text-[#0398d4] font-medium text-2xl">increaent & decrement</div>
+    <div className="bg-white flex justify-certer gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+      <button onClick={decreaseHandler} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">-</button>
+      <div className="font-bold gap-12 text-5xl">{count}</div>
+      <button onClick={increaseHandler}  className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">+</button>
+    </div>
+    <button onClick={resetHandler} className="bg-[#0398d4] text-white px-5 py-5 rounded-sm text-lg">Reset</button>
+   </div>
   );
 }
 
